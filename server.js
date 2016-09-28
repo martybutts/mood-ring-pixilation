db0613c95abe540afbb2401d0d0d58043c1f3435
Martin <martinbutts@clear.net.nz>
2016-09-28 23:39:32 +1300

    small changes to client.js

--- server.js
+++ server.js
@@ -2,27 +2,27 @@ var express = require('express')
 var path = require('path')
 var PORT = process.env.PORT || 3000
 
-var app = express()
+// var app = express()
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var routes = require('index.js');
+// var routes = require('index.js');
 
-app.get('/channel/:name', (req, res) => {
-  var channel = req.params.name
-
-  var nsp = io.of(`/${channel}`);
-
-  nsp.on('connection', function(socket){
-    console.log('someone connected')
-  });
-  nsp.emit('hi', 'everyone!');
-})
+// app.get('/channel/:name', (req, res) => {
+//   var channel = req.params.name
+//
+//   var nsp = io.of(`/${channel}`);
+//
+//   nsp.on('connection', function(socket){
+//     console.log('someone connected')
+//   });
+//   nsp.emit('hi', 'everyone!');
+// })
 
 //server setup
 
-routes(io)
+// routes(io)
 
 http.listen(PORT, function () {
   console.log('Server listening on port: ', PORT)
