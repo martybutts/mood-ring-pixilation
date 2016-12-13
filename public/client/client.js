2e56ab1f1335824d62418dba5794d8fdf77f8a26
Martin <martinbutts@clear.net.nz>
2016-12-14 10:09:06 +1300

    minor changes to client.js

--- public/client/client.js
+++ public/client/client.js
@@ -1,5 +1,5 @@
 var socket = io()
-const div = document.getElementsByTagName("div"); // selects the first div
+let div = document.getElementsByTagName("div")[0]; // selects the first div
 
 socket.on('connect', function () {
   console.log("socket on");
@@ -11,10 +11,10 @@ socket.on('color', function (data) {
   const { rgb } = data
   // change the color of the div
   div.style.backgroundColor = 'rgb(' + [rgb[0], rgb[1], rgb[2]].join(',') + ')'
-  if (div.nextElementSibling) // if a div is not last, move to the next one
+  while (div) // if a div is not last, move to the next one
     div = div.nextElementSibling
 
-  else // if the div is last move to the first one
-   div = div.parentElement.children[0]
+  // else // if the div is last move to the first one
+  //  div = div.parentElement.children[0]
 
 })
